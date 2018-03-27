745823c571588719c979bcff343648f6e436066d
NEVINSON\Nevinson <nevinson@outlook.com>
2018-03-27 17:28:50 +0200

    application user/dbcontext created and conficured....

---
+++ Angular.Core.Essentials/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Angular.Core.Essentials.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular.Core.Essentials.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
