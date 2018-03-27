adaec6abe5dacda8760913564a4f9b12ff9cdf68
NEVINSON\Nevinson <nevinson@outlook.com>
2018-03-27 19:53:55 +0200

    validation service created/implemented....

---
+++ Angular.Core.Essentials/ClientApp/app/services/validation.service.ts
@@ -0,0 +1,12 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidationService {
+
+    constructor() { }
+
+    validateEmail(email: string) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+}
