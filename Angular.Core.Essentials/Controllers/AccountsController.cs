254586877a67a9ca2fcafd202849a029adc9517d
NEVINSON\Nevinson <nevinson@outlook.com>
2018-03-27 18:19:44 +0200

    accounts controller/post logic implemented....

---
+++ Angular.Core.Essentials/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using Angular.Core.Essentials.Data;
+using Angular.Core.Essentials.Data.Models;
+using Angular.Core.Essentials.Helpers;
+using Angular.Core.Essentials.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Angular.Core.Essentials.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AccountsController(UserManager<ApplicationUser> userManager, IMapper mapper, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _db = context;
+        }
+
+        // POST api/accounts
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userIdentity = _mapper.Map<ApplicationUser>(model);
+
+            var result = await _userManager.CreateAsync(userIdentity, model.Password);
+
+            if (!result.Succeeded) return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
+
+            await _db.SaveChangesAsync();
+
+            return new OkObjectResult("Account created");
+        }
+    }
+}
