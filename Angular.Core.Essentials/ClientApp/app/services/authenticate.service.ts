a3d3d3d4e0e1fb72857d6c6730aecefb3b9364f3
NEVINSON\Nevinson <nevinson@outlook.com>
2018-03-27 19:06:11 +0200

    authenticate service created/implemented....

---
+++ Angular.Core.Essentials/ClientApp/app/services/authenticate.service.ts
@@ -0,0 +1,64 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { LoginViewModel } from '../viewmodels/login.viewmodel';
+import { RegisterViewModel } from '../viewmodels/register.viewmodel';
+
+@Injectable()
+export class AuthService {
+    authToken: any;
+    user: any;
+    authenticated: boolean;
+
+    constructor(private http: Http) { }
+
+    registerUser(user: RegisterViewModel) {
+        let headers = new Headers();
+        headers.append('Authorization', 'supersecretbulletproofkey');
+        headers.append('Content-Type', 'application/json');
+        let ep = 'api/Accounts/';
+
+        return this.http.post(ep, user, { headers: headers })
+            .map(res => res.json());
+    }
+
+    authenticateUser(user: LoginViewModel) {
+        // let body = `username=${user.username}&password=${user.password}&grant_type=password`
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let ep = 'api/Authenticate/';
+
+        return this.http.post(ep, user, { headers: headers })
+            .map(res => res.json());
+    }
+
+    storeUserData(token: string) { //, user
+        localStorage.setItem('access_token', token);
+        //localStorage.setItem('user', JSON.stringify(user));
+        this.authToken = token;
+        //this.user = user;
+        this.authenticated = true;
+    }
+
+    loadToken() {
+        const token = localStorage.getItem('access_token');
+        this.authToken = token;
+    }
+
+    loadUser() {
+        const user = localStorage.getItem('user');
+        this.user = user;
+    }
+
+    loggedIn() {
+        return this.authenticated; // tokenNotExpired();
+    }
+
+    logout() {
+        this.authToken = null;
+        this.user = null;
+        localStorage.clear();
+        this.authenticated = false;
+    }
+}
---
+++ Angular.Core.Essentials/ClientApp/app/viewmodels/register.viewmodel.ts
@@ -0,0 +1,7 @@
+export interface RegisterViewModel {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    location: string;
+}
